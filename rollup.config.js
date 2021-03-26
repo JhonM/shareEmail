2ed297ecfb8cada0ace284939f563ac808eba43b
JhonM <jhonmajoor@gmail.com>
2021-03-26 22:44:48 +0100

    Updated to TypeScript

--- rollup.config.js
+++ rollup.config.js
@@ -1,71 +1,37 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import sass from 'rollup-plugin-sass';
-import image from 'rollup-plugin-img';
+import { terser } from 'rollup-plugin-terser';
+
 import pkg from './package.json';
 
-const input = ['src/index.js'];
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-export default [
-  {
-    input,
-    plugins: [
-      nodeResolve(),
-      babel({
-        babelHelpers: 'bundled',
-      }),
-      terser(),
-    ],
-    output: {
-      file: `dist/${pkg.name}.min.js`,
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
       format: 'umd',
-      name: 'shareForm',
-      esModule: false,
-      exports: 'named',
-      sourcemap: true,
+      name: 'TscBoilerTmp',
     },
-  },
-  {
-    input,
-    plugins: [nodeResolve()],
-    output: [
-      {
-        dir: 'dist/esm',
-        format: 'esm',
-        exports: 'named',
-        sourcemap: true,
-      },
-      {
-        dir: 'dist/cjs',
-        format: 'cjs',
-        exports: 'named',
-        sourcemap: true,
-      },
-    ],
-  },
-  {
-    input: 'src/styles/input.js',
-    output: {
-      file: 'dist/output.js',
-      format: 'esm',
+    {
+      file: pkg.module,
+      format: 'es',
     },
-    plugins: [
-      sass({
-        output: true,
-        output: 'dist/bundle.css',
-      }),
-    ],
-  },
-  {
-    input: 'src/assets/index.js',
-    output: {
-      file: 'dist/assets/bundle.js',
-    },
-    plugins: [
-      image({
-        limit: 10000,
-      }),
-    ],
-  },
-];
+  ],
+  plugins: [
+    typescript({
+      rollupCommonJSResolveHack: true,
+      clean: true,
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions,
+    }),
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
+};
